cec66cecd01e5284880d23b7ed62ff10cfd40146
Yu Jin HONG <hyjno254@gmail.com>
2023-09-18 01:15:07 +0900

    [Silver I] Title: 동물원, Time: 72 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -7,7 +7,7 @@ n = int(input())
 if n == 1:
     print(3)
 else:
-    dp = [1 for _ in range(n+1)]
+    dp = [0]*(n+1)
     dp[1], dp[2] = 3, 7
     for i in range(3, n+1):
         dp[i] = (2 * dp[i-1] + dp[i-2]) % 9901
